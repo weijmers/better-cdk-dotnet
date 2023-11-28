cf8048838ac83ed7944a4f0a5147729da046e739
Daniel Weijmers <daniel@weijmers.com>
2023-11-28 10:51:00 +0100

    Initial commit

---
+++ src/BetterCdkDotnet/BetterCdkDotnetStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace BetterCdkDotnet
+{
+    public class BetterCdkDotnetStack : Stack
+    {
+        internal BetterCdkDotnetStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
